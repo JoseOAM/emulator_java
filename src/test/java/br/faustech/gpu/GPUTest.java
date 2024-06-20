40864f6ffe2da803dedda23db270e19196f4974f
Gabriel Faustino <gfaustino37@gmail.com>
2024-06-20 01:03:09 -0400

    feat: gpu test

---
+++ src/test/java/br/faustech/gpu/GPUTest.java
@@ -0,0 +1,44 @@
+package br.faustech.gpu;
+
+import br.faustech.bus.Bus;
+import br.faustech.memory.Memory;
+import java.lang.Thread.State;
+import org.junit.Test;
+
+
+public class GPUTest {
+
+  private static final String VIDEO_PATH = "";
+
+  @Test
+  public void gpuTest() {
+
+    int WIDTH = 512;
+    int HEIGHT = 512;
+
+    FrameBuffer frameBuffer = new FrameBuffer(WIDTH * HEIGHT * 8);
+
+    Memory memory = new Memory(1024);
+    Bus bus = new Bus(frameBuffer, memory);
+
+    GPU gpu = new GPU(WIDTH, HEIGHT, frameBuffer);
+    gpu.start();
+
+    VideoFrameToVertexArray videoProcessor = new VideoFrameToVertexArray(VIDEO_PATH,
+        WIDTH, HEIGHT, bus);
+    videoProcessor.start();
+
+    while (gpu.isAlive()) {
+      if (gpu.getState() == State.TERMINATED) {
+        // Stop all threads
+        if (videoProcessor.getState() != State.TERMINATED) {
+          videoProcessor.interrupt();
+        }
+        if (gpu.getState() != State.TERMINATED) {
+          gpu.interrupt();
+        }
+      }
+    }
+  }
+
+}
